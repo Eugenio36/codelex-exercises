e0f153d50a5bb9e844ec37fa5ee976396f021c18
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-02-23 19:55:18 +0200

    Exercise #2

--- src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
+++ src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
@@ -1,26 +1,41 @@
 package io.codelex.typesandvariables.practice;
 
+import java.text.DecimalFormat;
+
 public class MoreVariablesAndPrinting {
     public static void main(String[] args) {
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
+        String Name;
+        String Eyes;
+        String Teeth;
+        String Hair;
+        int Age;
+        int Height;
+        int Weight;
+        double HeightCm;
+        double WeightKg;
 
-        myName = "Zed A. Shaw";
-        myAge = 35;
-        myHeight = 74;  // inches
-        myWeight = 180; // lbs
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        Name = "Zed A. Shaw";
+        Age = 35;
+        Height = 74;
+        Weight = 180;
+        HeightCm = Height * 2.54;
+        WeightKg = Weight / 2.205;
+        Eyes = "Blue";
+        Teeth = "White";
+        Hair = "Brown";
 
-        System.out.println("Let's talk about " + myName + ".");
-        System.out.println("He's " + myHeight + " inches tall.");
-        System.out.println("He's " + myWeight + " pounds heavy.");
+        DecimalFormat decimal = new DecimalFormat("0.00");
+
+        System.out.println("Let's talk about " + Age + ".");
+        System.out.println("He's " + Height + " inches tall.");
+        System.out.println("He's " + Weight + " pounds heavy.");
+        System.out.println("He's " + decimal.format(HeightCm) + " cm tall.");
+        System.out.println("He's " + decimal.format(WeightKg) + " kg heavy.");
         System.out.println("Actually, that's not too heavy.");
-        System.out.println("He's got " + myEyes + " eyes and " + myHair + " hair.");
-        System.out.println("His teeth are usually " + myTeeth + " depending on the coffee.");
+        System.out.println("He's got " + Eyes + " eyes and " + Hair + " hair.");
+        System.out.println("His teeth are usually " + Teeth + " depending on the coffee.");
 
-        System.out.println("If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                + " I get " + (myAge + myHeight + myWeight) + ".");
+        System.out.println("If I add " + Age + ", " + Height + ", and " + Weight
+                + " I get " + (Age + Height + Weight) + ".");
     }
 }
--- src/main/java/io/codelex/typesandvariables/practice/SadFor.java
+++ src/main/java/io/codelex/typesandvariables/practice/SadFor.java
@@ -8,12 +8,14 @@ public class SadFor {
     }
 
     public static int test1() {
-//fixme
-//        for (int i = 0; i < 10; i++) {
-//            if (Math.sqrt(i) > 2.5)
-//                break;
-//        }
-//        return i;
+
+        for (int i = 0; i < 10; i++) {
+            if (Math.sqrt(i) < 2.5)
+                break;
+            return i;
+        }
+
+
         throw new NoCodeWrittenException();
     }
 }
