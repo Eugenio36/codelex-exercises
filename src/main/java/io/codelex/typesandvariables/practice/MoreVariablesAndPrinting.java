659b1ed1d3e2e1d43fd25444de45eee47b0eac4a
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-02-27 15:28:42 +0200

    MoreVariablesAndPrinting edited.

--- src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
+++ src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
@@ -4,38 +4,38 @@ import java.text.DecimalFormat;
 
 public class MoreVariablesAndPrinting {
     public static void main(String[] args) {
-        String Name;
-        String Eyes;
-        String Teeth;
-        String Hair;
-        int Age;
-        int Height;
-        int Weight;
-        double HeightCm;
-        double WeightKg;
+        String name;
+        String eyes;
+        String teeth;
+        String hair;
+        int age;
+        int height;
+        int weight;
+        double heightCm;
+        double weightKg;
 
-        Name = "Zed A. Shaw";
-        Age = 35;
-        Height = 74;
-        Weight = 180;
-        HeightCm = Height * 2.54;
-        WeightKg = Weight / 2.205;
-        Eyes = "Blue";
-        Teeth = "White";
-        Hair = "Brown";
+        name = "Zed A. Shaw";
+        age = 35;
+        height = 74;
+        weight = 180;
+        heightCm = height * 2.54;
+        weightKg = weight / 2.205;
+        eyes = "Blue";
+        teeth = "White";
+        hair = "Brown";
 
         DecimalFormat decimal = new DecimalFormat("0.00");
 
-        System.out.println("Let's talk about " + Age + ".");
-        System.out.println("He's " + Height + " inches tall.");
-        System.out.println("He's " + Weight + " pounds heavy.");
-        System.out.println("He's " + decimal.format(HeightCm) + " cm tall.");
-        System.out.println("He's " + decimal.format(WeightKg) + " kg heavy.");
+        System.out.println("Let's talk about " + age + ".");
+        System.out.println("He's " + height + " inches tall.");
+        System.out.println("He's " + weight + " pounds heavy.");
+        System.out.println("He's " + decimal.format(heightCm) + " cm tall.");
+        System.out.println("He's " + decimal.format(weightKg) + " kg heavy.");
         System.out.println("Actually, that's not too heavy.");
-        System.out.println("He's got " + Eyes + " eyes and " + Hair + " hair.");
-        System.out.println("His teeth are usually " + Teeth + " depending on the coffee.");
+        System.out.println("He's got " + eyes + " eyes and " + hair + " hair.");
+        System.out.println("His teeth are usually " + teeth + " depending on the coffee.");
 
-        System.out.println("If I add " + Age + ", " + Height + ", and " + Weight
-                + " I get " + (Age + Height + Weight) + ".");
+        System.out.println("If I add " + age + ", " + height + ", and " + weight
+                + " I get " + (age + height + weight) + ".");
     }
 }
