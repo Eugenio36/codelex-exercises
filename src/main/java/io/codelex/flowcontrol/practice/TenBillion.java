41d9137c137445690443e7f7b1f5a94c9ddb3e9f
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-03-07 13:07:09 +0200

    FlowControl - TenBillion exercise change.

--- src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
+++ src/main/java/io/codelex/flowcontrol/practice/TenBillion.java
@@ -21,23 +21,23 @@ public class TenBillion {
                 System.out.println("Number is greater or equals 10,000,000,000!");
             } else {
                 int digits = 1;
-                if (n >= 10 && n <= 99) {
+                if (n < 100) {
                     digits = 2;
-                } else if (n >= 100 && n <= 999) {
+                } else if (n < 1000) {
                     digits = 3;
-                } else if (n >= 1000 && n <= 9999) {
+                } else if (n < 10000) {
                     digits = 4;
-                } else if (n >= 10000 && n <= 99999) {
+                } else if (n < 100000) {
                     digits = 5;
-                } else if (n >= 100000 && n <= 999999) {
+                } else if (n < 1000000) {
                     digits = 6;
-                } else if (n >= 1000000 && n <= 9999999) {
+                } else if (n < 10000000) {
                     digits = 7;
-                } else if (n >= 10000000 && n <= 99999999) {
+                } else if (n < 100000000) {
                     digits = 8;
-                } else if (n >= 100000000 && n <= 999999999) {
+                } else if (n < 1000000000) {
                     digits = 9;
-                } else if (n >= 1000000000) {
+                } else if (n < 10000000000L) {
                     digits = 10;
                 }
                 System.out.println("Number of digits in the number: " + digits);
