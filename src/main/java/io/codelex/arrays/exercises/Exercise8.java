8cc7ea40c8fe656e02a6f0feee2a9b8d31c0dcd5
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-03-10 10:58:46 +0200

    "hangman" fixed.

--- src/main/java/io/codelex/arrays/exercises/Exercise8.java
+++ src/main/java/io/codelex/arrays/exercises/Exercise8.java
@@ -1,78 +1,92 @@
 package io.codelex.arrays.exercises;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Exercise8 {
 
-    public static void guess(String word, int life) {
-
-        char[] filler = new char[word.length()];
-        int i = 0;
-        while (i< word.length()) {
-            filler[i] = '-';
-            if (word.charAt(i) == ' ') {
-                filler[i] = ' ';
-            }
-            i++;
-        }
-        System.out.print(filler);
-        System.out.println("            Life remaining: " + life);
-        System.out.println();
-
-
-        Scanner scanner = new Scanner(System.in); // to read character from user
-        ArrayList<Character> list = new ArrayList<Character>();
-
-        while(life > 0) {
-            System.out.println("Please enter to letter: ");
-            char x = scanner.next().charAt(0);  // character input by user
-
-            if (list.contains(x)) {
-                System.out.println("Already entered");
-                continue;
-            }
-
-            list.add(x);
-
-            if (word.contains(x+"")) {                         // this loop will check all indexes for the
-                for (int y=0; y < word.length(); y++) {        // character and will replace '-' by
-                    if (word.charAt(y) == x) {                 // the character
-                        filler[y] = x;
-                    }
-                }
-            } else {
-                life--;  // life decreases if entered character is not in the word
-            }
-
-            if (word.equals(String.valueOf(filler))) {   //checking if filler == word, if yes, you won
-                System.out.println(filler);
-                System.out.println("You won! The secret word was: " + word);
-                break;
-            }
-
-            System.out.print(filler);
-            System.out.println("            Life remaining: " + life);
-            System.out.println();
-        }
-
-        if (life == 0) {
-            System.out.println("You lose!");
-        }
-
-    }
-
+    public static final String[] WORDS = {
+            "destination", "school", "attractive",
+            "belt", "clearance", "spontaneous",
+            "grimace", "leader", "executive",
+            "audience", "cold", "express",
+    };
 
     public static void main(String[] args) {
-
-        System.out.println("Hello! Try to guess the word!");
-        String word = "destination"; // word to be guessed
-        int life = 6; // number of chances
-        guess(word, life);
+        game();
     }
 
+    public static void game() {
+        Scanner scanner = new Scanner(System.in);
+
+        String guessedLetters = "";
+        String missedLetters = "";
+        String randomWord = WORDS[(int) (Math.random() * WORDS.length)];
+
+        int tries = 0;
+        while (tries <= randomWord.length()) {
+            printBoard(randomWord, guessedLetters, missedLetters);
+            String guess = scanner.nextLine();
+            if (randomWord.toUpperCase().contains(guess.toUpperCase())) {
+                if (!guessedLetters.toUpperCase().contains(guess.toUpperCase())) {
+                    guessedLetters += guess;
+                }
+            } else {
+                missedLetters += guess;
+                tries++;
+            }
+            if (playerWon(randomWord, guessedLetters)) {
+                System.out.println("YOU GOT IT!");
+                System.out.println("Your word is: " + randomWord);
+                break;
+            }
+        }
+
+        if (!playerWon(randomWord, guessedLetters)) {
+            System.out.println("You have lost!");
+        }
+
+        System.out.println();
+        System.out.println("Play 'again' or 'quit'? - yes/no");
+        String answer = scanner.nextLine();
+
+        if (answer.equals("yes") || answer.equals("y") || answer.equals("again") || answer.equals("a"))
+            game();
+        if (answer.equals("no") || answer.equals("n") || answer.equals("quit") || answer.equals("q"))
+            scanner.close();
     }
+
+
+    public static void printWord(String name, String guessedLetters) {
+        for (char a : name.toCharArray()) {
+            if (guessedLetters.toUpperCase().contains(String.valueOf(a).toUpperCase())) {
+                System.out.print(a);
+            } else {
+                System.out.print("_");
+            }
+        }
+        System.out.println();
+    }
+
+
+    public static boolean playerWon(String name, String guessedLetters) {
+        boolean hasWon = true;
+        for (char a : name.toCharArray()) {
+            if (!guessedLetters.contains(String.valueOf(a))) {
+                hasWon = false;
+                break;
+            }
+        }
+        return hasWon;
+    }
+
+    public static void printBoard(String name, String guessedLetters, String missedLetters) {
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+        System.out.print("Word: ");
+        printWord(name, guessedLetters);
+        System.out.println("Misses: " + missedLetters);
+        System.out.println("Guess: ");
+        System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+    }
+
+}
 
