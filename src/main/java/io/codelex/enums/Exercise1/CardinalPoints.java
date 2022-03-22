39656585745ae50c94798a992eca991d18dc75b3
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-03-22 09:36:09 +0200

    "Enums and maps" some changes.

--- src/main/java/io/codelex/enums/Exercise1/CardinalPoints.java
+++ src/main/java/io/codelex/enums/Exercise1/CardinalPoints.java
@@ -2,18 +2,30 @@ package io.codelex.enums.Exercise1;
 
 public enum CardinalPoints {
 
-    NORTH("up"),
-    SOUTH("down"),
-    EAST("right"),
-    WEST("left");
+    NORTH("North", "up", "0"),
+    SOUTH("South", "down", "1"),
+    EAST("East", "right", "2"),
+    WEST("West", "left", "3");
 
+    private final String name;
     private final String direction;
+    private final String value;
 
-    CardinalPoints(String direction) {
+    CardinalPoints(String name, String direction, String value) {
+        this.name = name;
         this.direction = direction;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getDirection() {
         return direction;
+    }
+
+    public String getValue() {
+        return value;
     }
 }
--- src/main/java/io/codelex/enums/Exercise1/CardinalPointsTest.java
+++ src/main/java/io/codelex/enums/Exercise1/CardinalPointsTest.java
@@ -3,10 +3,17 @@ package io.codelex.enums.Exercise1;
 public class CardinalPointsTest {
     public static void main(String[] args) {
 
-        int count = 0;
+        System.out.printf("%-10s %-5s\n", "Direction", "Text");
+        System.out.printf("%-10s %-5s\n", CardinalPoints.NORTH.getName(), CardinalPoints.NORTH.getDirection());
+        System.out.printf("%-10s %-5s\n", CardinalPoints.SOUTH.getName(), CardinalPoints.SOUTH.getDirection());
+        System.out.printf("%-10s %-5s\n", CardinalPoints.EAST.getName(), CardinalPoints.EAST.getDirection());
+        System.out.printf("%-10s %-5s\n", CardinalPoints.WEST.getName(), CardinalPoints.WEST.getDirection());
+
+        System.out.println("-------------------");
+
         for (CardinalPoints cardinalPoints : CardinalPoints.values()) {
-            System.out.println(cardinalPoints + ":" + cardinalPoints.getDirection() + ":" + count);
-            count++;
+            System.out.println(cardinalPoints.getName() + ":" + cardinalPoints.getDirection() + ":" + cardinalPoints.getValue());
+
         }
     }
 }
