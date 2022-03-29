3f4a51586d41844dc48abf2e96054c27002fbdfa
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-03-29 12:00:05 +0300

    "Main" pom.xml updated.

--- src/test/java/io/codelex/myExercises/AuthorTest.java
+++ src/test/java/io/codelex/myExercises/AuthorTest.java
@@ -1,26 +1,26 @@
-//package io.codelex.myExercises;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//import java.time.LocalDate;
-//
-//public class AuthorTest {
-//
-//    @Test
-//    void testToString() {
-//        // GIVEN
-//        Author author = new Author("John", "Petrov", LocalDate.now());
-//        String expectedToStringResult = "Author{firstName='John', lastName='Petrov', dateOfBirth=2022-03-23, book='[]'}";
-//
-//
-//        //WHEN
-//        String actualResult = author.toString();
-//
-//        //THEN
-//        Assertions.assertEquals(expectedToStringResult, actualResult);
-//
-//    }
-//
-//
-//}
+package io.codelex.myExercises;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class AuthorTest {
+
+    @Test
+    void testToString() {
+        // GIVEN
+        Author author = new Author("John", "Petrov", LocalDate.now());
+        String expectedToStringResult = "Author{firstName='John', lastName='Petrov', dateOfBirth=2022-03-23, book='[]'}";
+
+
+        //WHEN
+        String actualResult = author.toString();
+
+        //THEN
+        Assertions.assertEquals(expectedToStringResult, actualResult);
+
+    }
+
+
+}
