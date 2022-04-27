73836cc427daa8e55407200f9cecdbde59bf99cf
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-04-27 17:19:35 +0300

    "My exercises" push.

---
+++ src/main/java/io/codelex/myExercises/Factorial.java
@@ -0,0 +1,15 @@
+package io.codelex.myExercises;
+
+public class Factorial {
+
+    public static int factorial(int num) {
+        if (num == 0)
+            return 1;
+        else
+            return (num * factorial(num - 1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
