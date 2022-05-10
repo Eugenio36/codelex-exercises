87d2b87f534a39c0d54acc3cd6259153d0fd0114
WINDOWS-3C9PFI3\User <evgzlk@gmail.com>
2022-05-10 13:18:55 +0300

    "Own exercises".

--- src/main/java/io/codelex/myExercises/Test2.java
+++ src/main/java/io/codelex/myExercises/Test2.java
@@ -1,33 +1,7 @@
 package io.codelex.myExercises;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class Test2 {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\User\\IdeaProjects\\codelex-exercises\\src\\main\\java\\io\\codelex\\fileToRead.txt"));
-        String str;
-
-        ArrayList<String> list = new ArrayList<String>();
-        while ((str = reader.readLine()) != null) {
-            if (!str.isEmpty()) {
-                list.add(str);
-                System.out.println(str);
-            }
-        }
-        String[] stringArr = list.toArray(new String[0]);
-        System.out.println(Arrays.toString(stringArr));
-
-        
-        String joining = String.join(" ", stringArr);
-        String repl = joining.replace(".", "");
-        String[] splitting = repl.split("");
-        System.out.println(splitting.length);
-
+class Main {
+    public static void main(String[] args) {
     }
 }
 
